0fbc35815c73f33b95fc53bd6c2b4aa386cd9d4d
JuanDC10 <juancastpar@unisabana.edu.co>
2026-04-07 19:23:22 -0500

    3.1

--- dict_func.py
+++ dict_func.py
@@ -1,1 +1,5 @@
 # Write your code here!
+def employee_print(employee_info):
+    print("Name:", employee_info["Name"])
+    print("Salary:", employee_info["Salary"])
+    print("Role:", employee_info["Role"])
