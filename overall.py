be3a96526435433c8407f6c745e3f59c71fc725a
JuanDC10 <juancastpar@unisabana.edu.co>
2026-04-07 20:20:13 -0500

    5.5

--- overall.py
+++ overall.py
@@ -2,7 +2,21 @@ def student_averages(students):
     result = {}
     for student, grades in students.items():
         total = 0
+        count = 0 
         for score in grades.values():
             total += score
-        result[student] = total / len(grades)
+            count += 1
+        avg = round(total / count)
+        result[student] = avg
+    return result
+def assignment_averages(students):
+    result = {}
+    for assignment in list(students.values())[0].keys():
+        total = 0
+        count = 0
+        for grades in students.values():
+            total += grades[assignment]
+            count += 1
+        avg = round(total / count)
+        result[assignment] = avg
     return result
