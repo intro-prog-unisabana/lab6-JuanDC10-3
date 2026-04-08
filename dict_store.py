7ca209de12eb814913e3a6f4561f146bf9bcbaf5
JuanDC10 <juancastpar@unisabana.edu.co>
2026-04-07 19:07:49 -0500

    2

--- dict_store.py
+++ dict_store.py
@@ -1,1 +1,10 @@
 # Write your code here!
+def temp_and_color(data):
+    temp = None
+    color = None 
+    if "temp" in data:
+        temp = data["temp"]
+    if "color" in data:
+        color = data["color"]
+return (temp, color)
+
