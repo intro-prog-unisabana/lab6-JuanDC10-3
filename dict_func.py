8d77944ac38166eb229d6e3138441190cd9eb935
JuanDC10 <juancastpar@unisabana.edu.co>
2026-04-07 19:28:02 -0500

    3.2

--- dict_func.py
+++ dict_func.py
@@ -1,5 +1,15 @@
 # Write your code here!
 def employee_print(employee_info):
-    print("Name:", employee_info["Name"])
-    print("Salary:", employee_info["Salary"])
-    print("Role:", employee_info["Role"])
+    Name = None
+    Salary = None
+    Rol = None
+    if "Name" in employee_info:
+        Name = employee_info["Name"]
+    if "Salary" in employee_info:
+        Salary = employee_info["Salary"]
+    if "Rol" in employee_info:
+        Rol = employee_info["Rol"]
+    print("Name:", Name)
+    print("Salary:", Salary)
+    print("Rol:", Rol)
+    
