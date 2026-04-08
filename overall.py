c00e59ffcbe50d1c2acdc8c08e8c7f5a8fd00e1e
JuanDC10 <juancastpar@unisabana.edu.co>
2026-04-07 20:14:28 -0500

    5.3

--- overall.py
+++ overall.py
@@ -1,8 +1,8 @@
 def student_averages(students):
     result = {}
-    for student, grades in students:
+    for student, grades in students.items():
         total = 0
-        for score in grades:
+        for score in grades.values():
             total += score
         result[student] = total / len(grades)
     return result
