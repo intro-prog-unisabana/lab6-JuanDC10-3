4f700359e8538634114cdd7daf8ffba99eaaf707
JuanDC10 <juancastpar@unisabana.edu.co>
2026-04-07 19:03:06 -0500

    1.2

--- dict_concept.py
+++ dict_concept.py
@@ -1,7 +1,7 @@
 # Write your code here!
 def get_config():
     config = {
-        "widght": 480,
+        "width": 480,
         "height": 480,
         "color_mode": "dark",
         "sensitivity": 0.4
